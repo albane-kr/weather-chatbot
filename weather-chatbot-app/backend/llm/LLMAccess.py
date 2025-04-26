36c6427bc2ac16f49034006b51741b6cc9e1579f
albane-kr <riguidelly@gmail.com>
2025-04-26 19:35:16 +0200

    prompt works on Gemini but not via API

--- weather-chatbot-app/backend/llm/LLMAccess.py
+++ weather-chatbot-app/backend/llm/LLMAccess.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 # INSERT YOUR API KEY BELOW
-client = genai.Client(api_key=os.environ("GEMINI_API_KEY"))
-model = "gemini-1.5-flash"
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+model = "gemini-2.0-flash"
 def generate_response(prompt: str, language: str, weather_type: str, weather_type_intensity: str, temperature: str, geolocation: str, expression: str, emotion: str) -> str:
     """
     @param prompt: str -> This parameter is the textual user input
@@ -23,6 +24,8 @@ def generate_response(prompt: str, language: str, weather_type: str, weather_typ
     Description: This function takes the user input, adds some part between the | and then returns the textual response. 
     The length of this is limited to 10-30 words, to not significantly impact performance by too long responses.
     """
-    response = client.models.generate_content(model=model, content= prompt + f" | request: keep the answer between 10 and 30 words and answer in {language}! | Take into account that the answer should include the weather type {weather_type}, weather intensity {weather_type_intensity}, temperature {temperature}, geolocation {geolocation}, and this expression: {expression}, and with this emotion: {emotion}")
+    response = client.models.generate_content(model=model, contents = prompt + f" | request: keep the answer between 10 and 30 words and answer in {language}! | Take into account that the answer should include the weather type {weather_type}, weather intensity {weather_type_intensity}, temperature {temperature}, geolocation {geolocation}, and this expression: {expression}, and with this emotion: {emotion}. Do not fetch the real weather, assume the prediction in this prompt is correct")
     print(response)
     return response.text
+
+generate_response("Hello, what is the weather like today in Kerry?", "French", "rainy", "high", "20 degrees", "Berlin", "il pleut comme vache qui pisse", "sad")
